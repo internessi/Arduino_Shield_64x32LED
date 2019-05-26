df42fb32a354f0ec1b4a8e52048790c840cd16f9
internessi <internessi@users.noreply.github.com>
2019-05-26 15:57:34 +0200

    Update Arduino_Shield_64x32LED.ino

--- Arduino_Shield_64x32LED.ino
+++ Arduino_Shield_64x32LED.ino
@@ -29,11 +29,11 @@
 
 String        zeitanzeige, datumanzeige;
 String        menu_item [12];
-int           rotary_event_1 = 0, rotary_event_2 = 0;
-int           uhr_minuten, uhr_stunden, uhr_sekunden, uhr_tag, uhr_monat, uhr_jahr; // Uhr
-unsigned long currentTime, lastTime, letzteUhrZeit;
+int           rotary_event_1 = 0, rotary_event_2 = 0, SW1_event = 0, SW2_event = 0;
+int           uhr_minuten, uhr_stunden, uhr_sekunden, uhr_tag, uhr_monat, uhr_jahr, uhr_art; // Uhr
+unsigned long currentTime, lastTime, letzteUhrZeit, stoppuhr_wert = 0, stoppuhr_start = 0;
 boolean       encA, encB, encC, encD;
-boolean       lastAB = false, lastCD = false;
+boolean       lastAB = false, lastCD = false, stoppuhr_stopp = true;
 int           counter, selected_menu, selected_menu_item;
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
@@ -79,10 +79,7 @@ void setup() {
   letzteUhrZeit = currentTime; 
   
   Serial.begin(9600); 
-  
-  LED(1,1,1000);
-  LED(4,4,1000);
-  LED(2,2,1000);
+
   LED(7,7,1000);
 
   menu_item [0] = "PlaySnake";
@@ -152,18 +149,106 @@ void setup() {
   delay(500);
   matrix.fillScreen(matrix.Color333(0, 0, 0));
   counter = 0;
-  rotary_event_1 = -1;
-  selected_menu = 0;
-  selected_menu_item = 2;
-  PrintTime();
+//  rotary_event_1 = -1;
+  selected_menu = 1;
+  selected_menu_item = 1;
+  setSyncProvider(RTC.get); 
+  RESET_UHR();
+
 }
 
 void loop() {
   //delay(2500);
-if (selected_menu == 0) MENU();
-if (selected_menu == 1) UHR();
+  if (selected_menu == 0) MENU();
+  if (selected_menu == 1) STOPP_und_UHR();
+  if (selected_menu == 2) STOPP_und_UHR();
+  if (selected_menu == 3) STOPP_und_UHR();
+  if (selected_menu == 4) STOPP_und_UHR();
+  if (selected_menu == 5) STOPP_und_UHR();
+  if (selected_menu == 6) STOPP_und_UHR();
+  if (selected_menu == 7) STOPP_und_UHR();
+  if (selected_menu == 8) STOPP_und_UHR();
 
-ROTARY();
+  ROTARY();
+}
+
+void STOPP_und_UHR(){
+  
+  if (uhr_art == 0) UHR();
+  if (uhr_art == 1) STOPPUHR();
+  
+  SW_PUSH();          
+  if (SW1_event == 2){      // SW1 lang gedrückt?  
+    SW1_event = 0;                
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    selected_menu = 0;      // gehe zu Menu
+    rotary_event_1 = 1;     // Item + 1 und refresh
+    selected_menu_item = 0; // Menu Item 1 (0+1)
+  }
+}
+
+void STOPPUHR(){
+  UHR_ZEIT();  
+  STOPPUHR_ZEIT();
+  
+  if (SW2_event == 1){      // SW2 kurz gedrückt?  
+  SW2_event = 0;   
+  uhr_art = 0;
+  RESET_UHR();
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+  }
+}
+
+// stoppuhr_wert = 0, stoppuhr_start
+
+void STOPPUHR_ZEIT() {
+  currentTime = millis();
+  if(currentTime >= (letzteUhrZeit + 100)){
+            stoppuhr_wert = stoppuhr_start - millis();
+        matrix.setTextSize(1);   
+        matrix.setCursor(5, 22);   
+        matrix.print(stoppuhr_wert);   
+      if (stoppuhr_stopp){
+
+      }else{ 
+    
+      }    
+    }
+    
+  if (SW1_event > 1){      // SW1 gedrückt?  
+    SW1_event = 0;                
+    if (stoppuhr_stopp){
+      stoppuhr_stopp = false;
+      stoppuhr_start = millis();
+    }else{
+      stoppuhr_stopp = true;
+      stoppuhr_start = 0;   
+    }
+  }    
+    
+}
+
+void UHR() {
+  UHR_ZEIT();
+  UHR_DATUM();
+  if (SW2_event == 1){      // SW2 kurz gedrückt?  
+  SW2_event = 0;  
+  uhr_art = 1;
+  RESET_UHR();
+  matrix.fillScreen(matrix.Color333(0, 0, 0));
+  }
+}
+
+
+void SW_PUSH(){
+  if (digitalRead(SW1)){
+    currentTime = millis();
+    while (digitalRead(SW1)){}
+    if ((millis()- currentTime) < 600) SW1_event = 1; else SW1_event = 2;}
+  if (digitalRead(SW2)){
+    currentTime = millis();
+    while (digitalRead(SW2)){}
+    if ((millis()- currentTime) < 600) SW2_event = 1; else SW2_event = 2;}
 }
 
 void MENU(){
@@ -181,6 +266,14 @@ void MENU(){
       MENU_ITEMS();
       rotary_event_1 = 0;
       }    
+      
+  SW_PUSH();          
+  if (SW1_event > 0){      // SW1 gedrückt?  
+    SW1_event = 0;                
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    selected_menu = selected_menu_item;
+    RESET_UHR();
+  }      
 }
 
 void MENU_ITEMS(){
@@ -228,32 +321,11 @@ void MENU_pfeile(){
 }
 
 
-void UHR() {
+
+
+void UHR_DATUM() {
   currentTime = millis();
   if(currentTime >= (letzteUhrZeit + 1000)){
-    if(hour() != uhr_stunden){
-    int dezi_st = hour() / 10;
-    int basis_st = hour() - (dezi_st * 10);  
-    printUhrDigit(dezi_st,3,3,7,7,7,2,10,14);  
-    printUhrDigit(basis_st,14,3,7,7,7,2,10,14);     
-    uhr_stunden = hour(); 
-    matrix.setTextSize(2);
-    matrix.setCursor(21, 3);   
-    matrix.print(":");}
-    
-    if(minute() != uhr_minuten){
-    int dezi_min = minute() / 10;
-    int basis_min = minute() - (dezi_min * 10);
-    printUhrDigit(dezi_min,28,3,7,7,7,2,10,14);  
-    printUhrDigit(basis_min,39,3,7,7,7,2,10,14);     
-    uhr_minuten = minute();}
-    
-    if(second() != uhr_sekunden){
-    int dezi_sek = second() / 10;
-    int basis_sek = second() - (dezi_sek * 10);
-    printUhrDigit(dezi_sek,50,10,7,7,7,1,5,8);  
-    printUhrDigit(basis_sek ,56,10,7,7,7,1,5,8);     
-    uhr_sekunden = second();}
 
     if(day() != uhr_tag){
     int dezi_tag = day() / 10;
@@ -286,12 +358,36 @@ void UHR() {
     printUhrDigit(basis_jahr,54,22,7,7,7,1,5,8);     
     uhr_jahr = year();}  
   }
+}
 
+void UHR_ZEIT() {
+  currentTime = millis();
+  if(currentTime >= (letzteUhrZeit + 1000)){
 
-
-
-
-  
+    if(hour() != uhr_stunden){
+    int dezi_st = hour() / 10;
+    int basis_st = hour() - (dezi_st * 10);  
+    printUhrDigit(dezi_st,3,3,7,7,7,2,10,14);  
+    printUhrDigit(basis_st,14,3,7,7,7,2,10,14);     
+    uhr_stunden = hour(); 
+    matrix.setTextSize(2);
+    matrix.setCursor(21, 3);   
+    matrix.print(":");}
+    
+    if(minute() != uhr_minuten){
+    int dezi_min = minute() / 10;
+    int basis_min = minute() - (dezi_min * 10);
+    printUhrDigit(dezi_min,28,3,7,7,7,2,10,14);  
+    printUhrDigit(basis_min,39,3,7,7,7,2,10,14);     
+    uhr_minuten = minute();}
+    
+    if(second() != uhr_sekunden){
+    int dezi_sek = second() / 10;
+    int basis_sek = second() - (dezi_sek * 10);
+    printUhrDigit(dezi_sek,50,10,7,7,7,1,5,8);  
+    printUhrDigit(basis_sek ,56,10,7,7,7,1,5,8);     
+    uhr_sekunden = second();} 
+  }
 }
 
 
@@ -319,9 +415,7 @@ void ROTARY() {
 }
 
 void PrintTime() {
-
   setSyncProvider(RTC.get); //Dies ist die Funktion um die Zeit- und Datumsangabe von der RTC zu bekommen
-
   Serial.print(hour()); //Serial.print ist der Befehl etwas im seriellen Monitor anzuzeigen (Stunde, Minute, Sekunde, Leerzeichen, Tag, Leerzeichen, usw.)
   printDigits(minute()); //bei den Minuten und Sekunden wird der Befehl
   printDigits(second()); //printDigits angegeben welcher am Ende des Codes noch festgelegt wird.
@@ -387,4 +481,13 @@ Serial.print(":");
 if(digits < 10) 
 Serial.print('0'); 
 Serial.print(digits); 
+}
+
+void RESET_UHR(){
+  uhr_minuten = 0;
+  uhr_stunden = 0;
+  uhr_sekunden = 0;
+  uhr_tag = 0;
+  uhr_monat = 0;
+  uhr_jahr = 0;
 }
